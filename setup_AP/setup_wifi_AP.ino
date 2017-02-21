b5791b6e4e398843e9f6bff1de25d38d72a8191a
Arhimeas <arhimeas@gmail.com>
2017-02-21 13:14:25 +0100

    Add files via upload

---
+++ setup_AP/setup_wifi_AP.ino
@@ -0,0 +1,39 @@
+#include <ESP8266WiFi.h>
+
+//////////////////////
+// WiFi Definitions //
+//////////////////////
+const char WiFiAPPSK[] = "Sylvain_Amica"; // Ceci est le MDP de mon ESP
+
+WiFiServer server(80);
+
+void setup() 
+{
+  setupWiFi();
+  server.begin();
+}
+
+void loop() 
+{
+
+}
+
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+  String AP_NameString = "ESP8266 Thing " + macID;
+
+  char AP_NameChar[AP_NameString.length() + 1];
+  memset(AP_NameChar, 0, AP_NameString.length() + 1);
+
+  for (int i=0; i<AP_NameString.length(); i++)
+    AP_NameChar[i] = AP_NameString.charAt(i);
+
+  WiFi.softAP(AP_NameChar, WiFiAPPSK);
+}
